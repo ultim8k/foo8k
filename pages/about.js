b44682ae5e0f05b052f4c7c3786e319f8a23497c
Konstantinos Kapenekakis <ultimatekonstantinos@gmail.com>
2020-05-13 15:13:28 +0100

    Add Next.js

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+function AboutPage() {
+  return (
+    <div>
+      <h1>About us</h1>
+      <p>
+        <a>
+          Check <a href="/">home</a> page
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default AboutPage;
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+function HomePage() {
+  return (
+    <div>
+      <h1>Just another website</h1>
+      <p>
+        <a>
+          Check <a href="/about">about</a> page
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default HomePage;
